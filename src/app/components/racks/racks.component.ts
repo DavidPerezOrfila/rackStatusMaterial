ebba875bd1cab91593d0d8b73ef3c9fdd22d2c31
David Pérez <knightbcn@gmail.com>
2019-09-12 00:09:12 +0200

    updated angular 7 to 8

--- src/app/components/racks/racks.component.ts
+++ src/app/components/racks/racks.component.ts
@@ -24,8 +24,8 @@ export class RacksComponent implements OnInit {
   dataSource = new MatTableDataSource<Rack>(this.racks);
   displayedColumns: string[] = ['id', 'host', 'lat', 'lng', 'ico', 'img', 'info'];
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(private rackService: RackService) {}
   ngOnInit() {
